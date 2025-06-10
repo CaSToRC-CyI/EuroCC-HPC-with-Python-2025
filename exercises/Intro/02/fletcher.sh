03b302275e53176206a05b3d8e4f551dc1b803f6
cstyl <cstyl16@gmail.com>
2025-06-10 21:56:47 +0300

    Adding Intro exercises

---
+++ exercises/Intro/02/fletcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J fletcher32
+#SBATCH -o f32.txt
+#SBATCH -e f32.err
+#SBATCH -p cpu
+#SBATCH -A edu27
+#SBATCH --reservation=edu27
+#SBATCH -t 00:02:00
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+module load Python
+
+date +"%T.%6N"
+srun python fletcher32.py /nvme/scratch/edu27/intro/data/03.txt
+date +"%T.%6N"
